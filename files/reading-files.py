87f78503f3c57ccb8252f75c4ec2da5ceea63b56
Alex Kravchenko <krav4enko@mail.ru>
2020-01-05 00:53:04 +0300

    init reading files

---
+++ files/reading-files.py
@@ -0,0 +1,52 @@
+with open('recipes.txt', 'r', encoding='utf-8') as f:
+    cook_book = {}
+    dish, dish_number = None, 0
+    ingredients, ingredients_number = [], None
+    ingredients_use = 0
+
+
+    def split_ingredient(ingredient):
+        _ingredient = {}
+        _res = ingredient.split(' | ')
+        _ingredient['ingredient_name'] = _res[0]
+        _ingredient['quantity'] = _res[1]
+        _ingredient['measure'] = _res[2]
+        return _ingredient
+
+
+    for i, line in enumerate(f):
+        line = line.strip()
+        if i == dish_number:
+            dish = line
+        elif i == (dish_number + 1):
+            ingredients_number = int(line)
+            dish_number = ingredients_number + i + 2
+        else:
+            if line and (ingredients_use != ingredients_number):
+                res = split_ingredient(line)
+                ingredients.append(res)
+                ingredients_use = ingredients_use + 1
+            if ingredients_use == ingredients_number:
+                cook_book[dish] = ingredients
+                ingredients_use = 0
+                ingredients = []
+
+
+    def get_shop_list_by_dishes(dishes, person_count):
+        ingredients_list = {}
+        for _dish in dishes:
+            for ingredient in cook_book[_dish]:
+                if ingredient.get('ingredient_name') in ingredients_list:
+                    duplicate_ingredient = ingredients_list.get(ingredient.get('ingredient_name'))
+                    _quantity = duplicate_ingredient.get('quantity') + int(ingredient.get('quantity')) * person_count
+                else:
+                    _quantity = int(ingredient.get('quantity')) * person_count
+
+                item = dict(measure=ingredient.get('measure'),
+                            quantity=_quantity)
+                ingredients_list[ingredient.get('ingredient_name')] = item
+
+        print(ingredients_list)
+
+
+    get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2)
